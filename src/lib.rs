96c65baa841359e8d3c24c0c33315260366aaddb
dougefresh <dchimento@gmail.com>
2025-10-19 17:25:36 +0000

    add .env-example

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![doc = include_str!("../README.md")]
+
+mod error;
+mod lookup;
+mod transaction;
+use {borsh::BorshSerialize, solana_instruction::Instruction};
+pub use {error::*, lookup::*, nitrogen_instruction_builder::*, transaction::*};
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl From<Instruction> for TransactionBuilder {
+    fn from(instruction: Instruction) -> Self {
+        Self::builder().instructions(vec![instruction]).build()
+    }
+}
+
+impl<T: BorshSerialize> From<InstructionBuilder<T>> for TransactionBuilder {
+    fn from(builder: InstructionBuilder<T>) -> Self {
+        Self::from(builder.into_instruction())
     }
 }
